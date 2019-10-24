468f2f416c1514015ed4acd2ac10a76bfccdaa33
moharick <moharick@gmail.com>
2019-10-24 08:32:36 +0300

    This commit creates a profile model  class.

--- afrimma/models.py
+++ afrimma/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import datetime as dt
 
 # Create your models here.
+
+class Profile(models.Model):
+    Profile_photo = models.ImageField(upload_to = 'images/',blank=True)
+    Bio = models.TextField(max_length = 50,null = True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
+    score = models.ManyToManyField('Project', related_name='image',max_length=30)
+
+    def save_profile(self):
+        self.save()
+
+    @classmethod
+    def get_by_id(cls, id):
+        details = Profile.objects.get(user = id)
+        return details
+
+    @classmethod
+    def filter_by_id(cls, id):
+        details = Profile.objects.filter(user = id).first()
+        return details
+
+    @classmethod
+    def search_user(cls, name):
+        userprof = Profile.objects.filter(user__username__icontains = name)
+        return userprof
+
+    def save_profile(self):
+        self.save()
+
+    @classmethod
+    def get_by_id(cls, id):
+        details = Profile.objects.get(user = id)
+        return details
+
+    @classmethod
+    def filter_by_id(cls, id):
+        details = Profile.objects.filter(user = id).first()
+        return details
+
+    @classmethod
+    def search_user(cls, name):
+        userprof = Profile.objects.filter(user__username__icontains = name)
+        return userprof
