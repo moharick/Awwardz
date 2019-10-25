dde408e8ac44d694e35ce48c96868ff277cbb0a9
moharick <moharick@gmail.com>
2019-10-25 17:06:07 +0300

    This commit updates the model classes.

--- afrimma/models.py
+++ afrimma/models.py
@@ -7,46 +7,19 @@ import datetime as dt
 # Create your models here.
 
 class Profile(models.Model):
-    Profile_photo = models.ImageField(upload_to = 'images/',blank=True)
-    Bio = models.TextField(max_length = 50,null = True)
-    user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
-    score = models.ManyToManyField('Project', related_name='image',max_length=30)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    profile_photo = models.ImageField(upload_to='profiles/')
+    bio = models.CharField(max_length=255)
+    contact = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('user',)
+
+
 
     def save_profile(self):
         self.save()
 
-    @classmethod
-    def get_by_id(cls, id):
-        details = Profile.objects.get(user = id)
-        return details
-
-    @classmethod
-    def filter_by_id(cls, id):
-        details = Profile.objects.filter(user = id).first()
-        return details
-
-    @classmethod
-    def search_user(cls, name):
-        userprof = Profile.objects.filter(user__username__icontains = name)
-        return userprof
-
-    def save_profile(self):
-        self.save()
-
-    @classmethod
-    def get_by_id(cls, id):
-        details = Profile.objects.get(user = id)
-        return details
-
-    @classmethod
-    def filter_by_id(cls, id):
-        details = Profile.objects.filter(user = id).first()
-        return details
-
-    @classmethod
-    def search_user(cls, name):
-        userprof = Profile.objects.filter(user__username__icontains = name)
-        return userprof
 
 class Project(models.Model):
     title = models.CharField(max_length=150)
