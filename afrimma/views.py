7cac56f78c754aa20cbd9765927a869cf49a7883
moharick <moharick@gmail.com>
2019-10-30 11:54:46 +0300

    this commit updates views.

--- afrimma/views.py
+++ afrimma/views.py
@@ -1,23 +1,28 @@
-from django.shortcuts import render,redirect,get_object_or_404
-from django.http import HttpResponse,Http404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, Http404
 import datetime as dt
 from django.contrib.auth import login, authenticate
 from .models import *
 from django.contrib.auth.decorators import login_required
 from .forms import *
+from django.template import RequestContext
 from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
+
+
 def convert_dates(dates):
     # function that gets the weekday number for the date.
     day_number = dt.date.weekday(dates)
 
-    days = ['Monday','Tuesday','Wednesday','thursday','Friday','Saturday','Sunday']
+    days = ['Monday', 'Tuesday', 'Wednesday',
+            'thursday', 'Friday', 'Saturday', 'Sunday']
     '''
     Returns the actual day of the week
     '''
     day = days[day_number]
     return day
+
 
 def signup(request):
     if request.method == 'POST':
@@ -33,6 +38,7 @@ def signup(request):
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
 
+
 @login_required(login_url='/accounts/login')
 def home(request):
     current_user = request.user
@@ -46,6 +52,7 @@ def home(request):
         return redirect('edit')
     return render(request, 'home.html', locals())
 
+
 @login_required(login_url='/accounts/login')
 def upload(request):
     current_user = request.user
@@ -56,7 +63,104 @@ def upload(request):
             image = form.save(commit=False)
             image.user = current_user
             image.save()
-            return redirect('home')
+            return redirect('home', context_instance=RequestContext(request))
     else:
         form = UploadForm()
     return render(request, 'upload.html', {'form': form, 'profile': profile})
+
+
+@login_required(login_url='/accounts/login')
+def profile(request):
+    current_user = request.user
+    profile = Profile.objects.filter(user=current_user)
+    print(profile)
+    projects = Project.objects.filter(user=current_user)
+    my_profile = Profile.objects.get(user=current_user)
+    return render(request, 'profile.html', locals())
+
+
+@login_required(login_url='/accounts/login')
+def project(request, project_id):
+    current_user = request.user
+    profile = Profile.objects.get(user=current_user)
+    try:
+        project = Project.objects.get(id=project_id)
+    except Project.DoesNotExist:
+        raise ObjectDoesNotExist()
+
+    total_design = 0
+    total_usability = 0
+    total_content = 0
+    overall_score = 0
+
+    ratings = Review.objects.filter(project=project_id)
+    if len(ratings) > 0:
+        users = len(ratings)
+    else:
+        users = 1
+
+    design = list(Review.objects.filter(
+        project=project_id).values_list('design', flat=True))
+    usability = list(Review.objects.filter(
+        project=project_id).values_list('usability', flat=True))
+    content = list(Review.objects.filter(
+        project=project_id).values_list('content', flat=True))
+
+    total_design = sum(design)/users
+    total_usability = sum(usability)/users
+    total_content = sum(content)/users
+
+    overall_score = (total_design+total_content+total_usability)/3
+
+    project.design = total_design
+    project.usability = total_usability
+    project.content = total_content
+    project.overall = overall_score
+    project.save()
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, request.FILES)
+        if form.is_valid():
+            rating = form.save(commit=False)
+            rating.project = project
+            rating.profile = profile
+            if not Review.objects.filter(profile=profile, project=project).exists():
+                rating.overall_score = (
+                    rating.design+rating.usability+rating.content)/3
+                rating.save()
+    else:
+        form = ReviewForm()
+    return render(request, "review.html", {"project": project, "profile": profile, "ratings": ratings, "form": form, 'total_design': total_design, 'total_usability': total_usability, 'total_content': total_content})
+
+
+@login_required(login_url='/accounts/login/')
+def edit_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, request.FILES)
+        if form.is_valid():
+            prof = form.save(commit=False)
+            prof.user = current_user
+            prof.save()
+            return redirect('profile')
+    else:
+        form = ProfileUpdateForm()
+    return render(request, 'edit_profile.html', {'form': form})
+
+
+@login_required(login_url='/accounts/login')
+def search(request):
+    current_user = request.user
+    profile = Profile.objects.get(user=current_user)
+    if 'project' in request.GET and request.GET["project"]:
+        search_term = request.GET.get("project")
+        projects = Project.search_project(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html', {"message": message, "projects": projects, 'profile': profile})
+    else:
+        message = "Please enter search term"
+        return render(request, 'search.html', {"message": message, "projects": projects, 'profile': profile})
+
+def logout(request):
+    return redirect('home.html')
